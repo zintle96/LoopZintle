a88fd4cfc7734f97b964299f6b1facb5c8d8968a
Zintle Meslane <meslanezintle@gmail.com>
2021-06-15 12:53:45 +0200

    testing loops

--- test.js
+++ test.js
@@ -1,8 +1,68 @@
-var age = ['16', '20', '24', '28', '32', '36'];
-var y;
 
-for (var y = 3; y <= age.length; y++) {
-    console.log(age[y])
+/*testing for loops*/
+for (var z = 0; z <= 20; z++) {
+console.log(z)
 };
 
 
+var drinks = ['coffee', 'tea', 'soda', 'water', 'tequila', 'cider', 'lemon juice']
+var y;
+for (var y = 0; y < drinks.length; y++) {
+    console.log(drinks[y])
+};
+
+/*testing while loops*/
+x = 0
+while (x < 1000) {
+    console.log(x);
+    x++
+};
+
+a = 0
+while (a < 2000) {
+    console.log(a);
+    a = a + 2
+};
+
+/*testing arrays*/
+cars = ['BMW', 'VW', 'TOYOTA', 'BUGATTI'];
+// console.log(cars[0]);
+cars.forEach (item => {
+    console.log(item);
+});
+
+
+/*array of objects*/
+car =[{
+    carName:'bmw',
+    carAge :5,
+    carColor :'blue',
+},
+{
+    carName:'vw',
+    carAge :10,
+    carColor :'yellow',
+},
+{
+    carName: 'audi',
+    carAge: '7',
+    carColor: 'red',
+}
+]
+
+// console.log(car[2].carName +" "+car[2].carAge + " " + car[1].carColor)
+
+car.forEach(element => {
+    console.log(element.carName +" "+ element.carAge + " " + element.carColor)
+});
+
+/*variables and objects*/
+plates = 
+
+cars = ["BMW", "Toyota",{Plates: ["CAAT", "CAF288"]}];
+cars.forEach (item => {
+console.log(item);
+    if(item.Plates !== undefined){
+        console.log(item.Plates)
+    }
+});
